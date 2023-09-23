84f497f1b34aa6c8e9b15e06dcddc97e7481e737
taguru <mohammed.taj@halan.com>
2023-09-23 19:06:26 +0300

    - fix typo

--- remote/src/main/java/com/task/remote/di/services/sortedGitRepositories/model/GithubRepositoriesResponse.kt
+++ remote/src/main/java/com/task/remote/di/services/sortedGitRepositories/model/GithubRepositoriesResponse.kt
@@ -2,7 +2,7 @@ package com.task.remote.di.services.sortedGitRepositories.model
 
 import com.task.domain.domain_module.sortedGitRepositories.models.GithubRepositoriesDomain
 
-data class GithubRepositoriesResponses(
+data class GithubRepositoriesResponse(
     val incomplete_results: Boolean,
     val items: List<ItemResponse>,
     val total_count: Int
